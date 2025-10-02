8052e412eff43f126a7089d6856233518df3937b
Krishna Nikhil Kindi <153883926+NikhilKindi@users.noreply.github.com>
2025-10-02 16:21:27 -0400

    Time: 38 ms (65.55%), Space: 17.7 MB (57.2%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,23 +1,19 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        res=0
         k=x
-        
-        if k<0: 
+        d=0
+        if k<0:
             k=abs(k)
         while k!=0:
-            y=k%10
-            res=res*10+y
-            k=k//10
-               
-        if res>(2**31)-1:
+            r= k % 10
+            d = r + 10 * d
+            k= k//10
+
+        if d> (2**31)-1:
             return 0
         elif x>0:
-            return res
+            return d
         else:
-            return -1*res
-            
-    
-    
-            
+            return -1*d
+
         
